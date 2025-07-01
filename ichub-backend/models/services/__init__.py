3d8a58526f91c94f11e2f1e7322820759b9a4644
Michael Scholz <michael.scholz@draexlmaier.com>
2025-07-01 16:24:19 +0200

    Updated __init__.py for service models

--- ichub-backend/models/services/__init__.py
+++ ichub-backend/models/services/__init__.py
@@ -63,6 +63,8 @@ from .twin_management import (
     CatalogPartTwinDetailsRead,
     BatchTwinCreate,
     JISPartTwinCreate,
-    SerializedPartTwinCreate
+    SerializedPartTwinCreate,
+    SerializedPartTwinRead,
+    SerializedPartTwinDetailsRead,
 )
 
