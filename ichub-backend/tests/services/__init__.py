14a69bfa4047b3dcac0fce0291f06d2527742ac0
Mikel Garcia <mgarcia@lksnext.com>
2025-09-03 09:21:41 +0200

    Update copyright information in __init__.py

--- ichub-backend/tests/services/__init__.py
+++ ichub-backend/tests/services/__init__.py
@@ -1,6 +1,7 @@
 #################################################################################
 # Eclipse Tractus-X - Industry Core Hub Backend
 #
+# Copyright (c) 2025 LKS NEXT
 # Copyright (c) 2025 Contributors to the Eclipse Foundation
 #
 # See the NOTICE file(s) distributed with this work for additional
