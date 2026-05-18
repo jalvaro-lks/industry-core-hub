b3ba46302a7a0ec277c8f67829311a377ab41504
Carlos Diez <cdiez@lksnext.com>
2026-05-18 14:35:49 +0200

    chore: remove unused import from certificates.py and add light blue gradient variable in _variables.scss

--- ichub-backend/models/services/addons/ccm_kit/v1/certificates.py
+++ ichub-backend/models/services/addons/ccm_kit/v1/certificates.py
@@ -21,7 +21,6 @@
 #################################################################################
 
 from typing import Optional
-from datetime import datetime
 from pydantic import BaseModel, Field
 
 
